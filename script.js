e80d6f49872a19aecbfe3f2b17da4c1cceed65ee
Shreyas Srikanth <sshreyas567@gmail.com>
2023-11-29 06:05:20 +0530

    comment display fixed

--- script.js
+++ script.js
@@ -12,11 +12,21 @@ async function fetchItems() {
     }
 }
 
+async function fetchComments(id) {
+    try {
+        const response = await axios.get(`http://localhost:4000/comments/${id}`);
+        return response.data;
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
+}
+
 async function displayData(post) {
     const itemList = document.createElement('ul');
     itemList.id = 'item-list';
 
-    post.forEach(item => {
+    for (const item of post) {
         const listItem = document.createElement('li');
         listItem.classList.add('item');
 
@@ -31,28 +41,6 @@ async function displayData(post) {
 
         const commentButton = document.createElement('button');
         commentButton.textContent = 'Comment';
-
-        commentButton.addEventListener('click', function() {
-            const commentInput = document.createElement('input');
-            commentInput.type = 'text';
-            commentInput.placeholder = 'Enter your comment';
-
-            const sendButton = document.createElement('button');
-            sendButton.textContent = 'Send';
-
-            sendButton.addEventListener('click', function() {
-                const commentText = commentInput.value;
-                // Process the commentText 
-                console.log(commentText);
-                 updateQuantity(item.id,commentText)
-                // Remove the comment input and send button afterward
-                listItem.removeChild(commentInput);
-                listItem.removeChild(sendButton);
-            });
-
-            listItem.appendChild(commentInput);
-            listItem.appendChild(sendButton);
-        });
 
         listItem.style.background = 'lightblue';
         listItem.style.textAlign = 'center';
@@ -69,35 +57,99 @@ async function displayData(post) {
         listItem.appendChild(commentButton);
 
         itemList.appendChild(listItem);
-    });
+
+        commentButton.addEventListener('click', async function (e) {
+            e.preventDefault();
+
+            const listItem = this.parentElement;
+            let commentInput = listItem.querySelector('.comment-input');
+            let sendButton = listItem.querySelector('.send-button');
+
+            if (!commentInput && !sendButton) {
+                commentInput = document.createElement('input');
+                commentInput.type = 'text';
+                commentInput.placeholder = 'Enter your comment';
+                commentInput.classList.add('comment-input');
+
+                sendButton = document.createElement('button');
+                sendButton.textContent = 'Send';
+                sendButton.classList.add('send-button');
+
+                sendButton.addEventListener('click', async function (e) {
+                    e.preventDefault();
+                    const commentText = commentInput.value;
+                    await updateQuantity(item.id, commentText);
+                    commentInput.value = '';
+
+                    try {
+                        const comment = await fetchComments(item.id);
+                        displayComments(comment, listItem, true);
+                    } catch (error) {
+                        console.log(error);
+                    }
+                });
+
+                
+
+                listItem.appendChild(commentInput);
+                listItem.appendChild(sendButton);
+
+                const commentsContainer = document.createElement('div');
+                commentsContainer.classList.add('comments-container');
+                listItem.appendChild(commentsContainer);
+            }
+
+            try {
+                const comment = await fetchComments(item.id);
+                displayComments(comment, listItem, false);
+            } catch (error) {
+                console.log(error);
+            }
+        });
+    }
 
     document.body.appendChild(itemList);
 }
 
 
-async function updateQuantity(id,comment){
-    console.log(id,comment);
-    await axios.post('http://localhost:4000/items/updateComment',{
-        id:id,
-        comment:comment,
-        name:'Anonymous'
-    })
+async function updateQuantity(id, comment) {
+    try {
+        await axios.post('http://localhost:4000/items/updateComment', {
+            id: id,
+            comment: comment,
+            name: 'Anonymous'
+        });
+    } catch (err) {
+        console.log(err);
+    }
 }
 
+function displayComments(comments, listItem, shouldDisplayComments) {
+    let commentsContainer = listItem.querySelector('.comments-container');
 
-fetchItems()
-    .then(response => displayData(response))
-    .catch(err => {
-        console.log(err);
+    if (!commentsContainer) {
+        commentsContainer = document.createElement('div');
+        commentsContainer.classList.add('comments-container');
+        listItem.appendChild(commentsContainer);
+    } else {
+        commentsContainer.innerHTML = '';
+    }
+
+    comments.forEach(comment => {
+        const commentElement = document.createElement('p');
+        commentElement.textContent = comment.name + ' = ' + comment.comments;
+        commentsContainer.appendChild(commentElement);
     });
 
-async function addToExpress(e) {
+    if (shouldDisplayComments && !listItem.contains(commentsContainer)) {
+        listItem.appendChild(commentsContainer);
+    }
+}
 
+async function addToExpress() {
+   
     let url = document.getElementById('url').value;
     let caption = document.getElementById('caption').value;
-
-    console.log(url);
-    console.log(caption);
 
     try {
         const response = await axios.post('http://localhost:4000/items', {
@@ -105,16 +157,12 @@ async function addToExpress(e) {
             caption: caption
         });
 
-        console.log("Response from POST:", response);
-
         alert("Data successfully Saved");
 
         document.getElementById('url').value = "";
-        document.getElementById('caption').value = ""; 
-        
+        document.getElementById('caption').value = "";
 
-        const newData = await fetchItems(); 
-        console.log("New Data:", newData);
+        const newData = await fetchItems();
         displayData(newData);
     } catch (err) {
         console.log(err);
@@ -122,5 +170,8 @@ async function addToExpress(e) {
     }
 }
 
-
-
+fetchItems()
+    .then(response => displayData(response))
+    .catch(err => {
+        console.log(err);
+    });
